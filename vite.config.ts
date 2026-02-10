fc0a14199746d8ecfe23bf638df697ad69c005a3
Frank van Wijk <info@frankvanwijk.nl>
2026-02-10 10:07:35 +0100

    Add PWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,46 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        background_color: '#0f172b',
+        description: 'Musician companion app for managing songs and setlists',
+        display: 'standalone',
+        icons: [
+          {
+            sizes: '192x192',
+            src: 'pwa-192x192.png',
+            type: 'image/png',
+          },
+          {
+            sizes: '512x512',
+            src: 'pwa-512x512.png',
+            type: 'image/png',
+          },
+          {
+            purpose: 'any maskable',
+            sizes: '512x512',
+            src: 'pwa-512x512.png',
+            type: 'image/png',
+          },
+        ],
+        name: 'BandBuddy',
+        short_name: 'BandBuddy',
+        theme_color: '#009966',
+      },
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2}'],
+      },
+    }),
+  ],
   server: {
     host: '127.0.0.1',
   },
